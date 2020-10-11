f123471f95c72b935a66c7cd2d88a2959c539975
Wayne Duran <asartalo@gmail.com>
2020-10-11 19:43:09 +0800

    Text to Clipboard

---
+++ javascript/textToClipboard.js
@@ -0,0 +1,8 @@
+function textToClipboard (text) {
+  let dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+  dummy.value = text;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+}
